fa63f20cb5f045e20d4b2dc506a7ccbfeafa83af
Mario Lopez <jlopezvillarreal@my.bcit.ca>
2023-01-27 09:36:06 -0800

    mario-changes

---
+++ scripts/ml-scripts.js
@@ -0,0 +1,18 @@
+// JavaScript Document
+
+// Responsive Menu - Dropdown
+const body = document.body;
+const btnMenu = document.getElementById('btn-menu');
+const bar = document.getElementById('bar');
+const nav = document.getElementById('main-navigation');
+
+btnMenu.addEventListener('click', openMenu);
+// Prevents the focus state from activating
+btnMenu.addEventListener('mousedown', function (e) {
+  e.preventDefault();
+});
+
+function openMenu() {
+  // Do some open/close nav stuff here...
+  nav.classList.toggle('show');
+}
